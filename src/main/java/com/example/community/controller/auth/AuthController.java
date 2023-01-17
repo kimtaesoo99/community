7c01c08f8c021a09f8660239cd6ccc8c4827eec7
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-18 01:11:40 +0900

    feat: 로그인 관련 컨트롤러 추가

---
+++ src/main/java/com/example/community/controller/auth/AuthController.java
@@ -0,0 +1,46 @@
+package com.example.community.controller.auth;
+
+import com.example.community.dto.sign.LoginRequestDto;
+import com.example.community.dto.sign.SignUpRequestDto;
+import com.example.community.dto.sign.TokenRequestDto;
+import com.example.community.response.Response;
+import com.example.community.service.auth.AuthService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static com.example.community.response.Response.success;
+
+@Api(value = "Sign Controller", tags = "Sign")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+    private final AuthService authService;
+
+    @ApiOperation(value = "회원가입", notes = "회원가입 진행")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/sign-up")
+    public Response register(@Valid @RequestBody SignUpRequestDto signUpRequestDto) {
+        authService.signup(signUpRequestDto);
+        return success();
+    }
+
+    @ApiOperation(value = "로그인", notes = "로그인을 한다.")
+    @PostMapping("/sign-in")
+    @ResponseStatus(HttpStatus.OK)
+    public Response signIn(@Valid @RequestBody LoginRequestDto req) {
+        return success(authService.signIn(req));
+    }
+
+    @ApiOperation(value = "토큰 재발급", notes = "토큰 재발급 요청")
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/reissue")
+    public Response reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        return success(authService.reissue(tokenRequestDto));
+    }
+}
