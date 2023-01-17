f71b1a1bfddc535b3bb3d2e9c10b2056e083da1f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-18 01:21:54 +0900

    feat: 토큰 요청 DTO 추가

---
+++ src/main/java/com/example/community/dto/sign/TokenRequestDto.java
@@ -0,0 +1,15 @@
+package com.example.community.dto.sign;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TokenRequestDto {
+    private String accessToken;
+    private String refreshToken;
+}
