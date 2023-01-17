edc7123dd72032ed5caa8e04f0474ef8d4f4acbb
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-18 01:12:41 +0900

    feat: 서로 다른 서버 환경에서 자원 공유 설정 추가

---
+++ src/main/java/com/example/community/config/security/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.community.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
