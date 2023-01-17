56f3c80f9cc7df669613d7e69125f39d067b2a91
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-18 01:20:24 +0900

    feat: 회원가입시 요청 DTO 추가

---
+++ src/main/java/com/example/community/dto/sign/SignUpRequestDto.java
@@ -0,0 +1,32 @@
+package com.example.community.dto.sign;
+
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@ApiModel(value = "회원가입 요청")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpRequestDto {
+    @ApiModelProperty(value = "아이디", notes = "아이디를 입력해주세요", required = true, example = "kimtaesoo")
+    @NotBlank(message = "아이디를 입력해주세요.")
+    private String username;
+
+    @ApiModelProperty(value = "비밀번호", required = true, example = "123456")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+//    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", message = "비밀번호는 최소 8자리이면서 1개 이상의 알파벳, 숫자, 특수문자를 포함해야합니다.")
+    private String password;
+
+    @ApiModelProperty(value = "사용자 이름", notes = "사용자 이름은 한글 또는 알파벳으로 입력해주세요.", required = true, example = "김태수")
+    @NotBlank(message = "사용자 이름을 입력해주세요.")
+    @Size(min = 2, message = "사용자 이름이 너무 짧습니다.")
+    private String name;
+
+}
