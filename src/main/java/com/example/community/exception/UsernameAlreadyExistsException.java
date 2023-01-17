4509f3920e9ef3f9e68506255c3af416245c4aa8
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-18 01:22:23 +0900

    feat: 유저이름이 겹칠때 에러를 발생시켜주는 클래스 추가

---
+++ src/main/java/com/example/community/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.community.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException{
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
