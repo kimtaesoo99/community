c59343f75b0275f15f8088eb0404efc93f11b330
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-19 00:49:13 +0900

    feat: 회원 조회 기능 추가

--- src/main/java/com/example/community/controller/member/MemberController.java
+++ src/main/java/com/example/community/controller/member/MemberController.java
@@ -1,11 +1,15 @@
 package com.example.community.controller.member;
 
 
+import com.example.community.repository.member.MemberRepository;
+import com.example.community.response.Response;
 import com.example.community.service.member.MemberService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Api(value = "Member Controller", tags = "Member")
 @RequestMapping("/api")
@@ -14,6 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
+
+    @ApiOperation(value = "회원 전체 조회", notes = "회원 전체 조회")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/members")
+    public Response findAllMembers(){
+        return Response.success(memberService.findAllMembers());
+    }
+
+    @ApiOperation(value = "회원 개별 조회", notes = "회원 개별 조회")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/members/{id}")
+    public Response findMember(@ApiParam(value = "Member Id",required = true)
+                                   @PathVariable("id") Long id){
+        return Response.success(memberService.findMember(id));
+    }
 
 
 }
--- src/main/java/com/example/community/service/member/MemberService.java
+++ src/main/java/com/example/community/service/member/MemberService.java
@@ -1,7 +1,34 @@
 package com.example.community.service.member;
 
+import com.example.community.domain.member.Member;
+import com.example.community.dto.member.MemberFindResponseDto;
+import com.example.community.exception.MemberNotFoundException;
+import com.example.community.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
 @Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
+    public List<MemberFindResponseDto> findAllMembers() {
+        List<Member> members = memberRepository.findAll();
+        List<MemberFindResponseDto> result = members.stream()
+            .map(member -> MemberFindResponseDto.toDto(member))
+            .collect(Collectors.toList());
+        return result;
+    }
+
+    @Transactional(readOnly = true)
+    public MemberFindResponseDto findMember(Long id) {
+        Member member = memberRepository.findById(id).orElseThrow(MemberNotFoundException::new);
+        return MemberFindResponseDto.toDto(member);
+    }
+
 }
