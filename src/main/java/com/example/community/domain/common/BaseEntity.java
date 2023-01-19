1c4b52171867458396e881053ccde9e9ee88c8a0
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-19 16:03:17 +0900

    feat: 값을 저장해주는 메서드 추가

--- src/main/java/com/example/community/domain/common/BaseEntity.java
+++ src/main/java/com/example/community/domain/common/BaseEntity.java
@@ -5,9 +5,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
@@ -21,5 +19,18 @@ public abstract class BaseEntity {
     @LastModifiedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime modifiedAt;
+
+    /* 해당 엔티티를 저장하기 이전에 실행 */
+    @PrePersist
+    public void onPrePersist(){
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = this.createdAt;
+    }
+
+    /* 해당 엔티티를 업데이트 하기 이전에 실행*/
+    @PreUpdate
+    public void onPreUpdate(){
+        this.modifiedAt = LocalDateTime.now();
+    }
 }
 
