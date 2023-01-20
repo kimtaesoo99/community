7f9a28016c2054a69d4569f501535ceb75619e0f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-20 14:02:57 +0900

    feat: 회원 정보를 수정, 탈퇴 추가

--- src/main/java/com/example/community/service/member/MemberService.java
+++ src/main/java/com/example/community/service/member/MemberService.java
@@ -1,10 +1,12 @@
 package com.example.community.service.member;
 
 import com.example.community.domain.member.Member;
+import com.example.community.dto.member.MemberEditRequestDto;
 import com.example.community.dto.member.MemberFindResponseDto;
 import com.example.community.exception.MemberNotFoundException;
 import com.example.community.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,7 @@ import java.util.stream.Collectors;
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional(readOnly = true)
     public List<MemberFindResponseDto> findAllMembers() {
@@ -28,6 +31,18 @@ public class MemberService {
     public MemberFindResponseDto findMember(Long id) {
         Member member = memberRepository.findById(id).orElseThrow(MemberNotFoundException::new);
         return MemberFindResponseDto.toDto(member);
+    }
+
+    @Transactional
+    public void editMemberInfo(Member member,MemberEditRequestDto memberEditRequestDto) {
+        member.modify(
+            passwordEncoder.encode(memberEditRequestDto.getPassword()),
+            memberEditRequestDto.getName());
+    }
+
+    @Transactional
+    public void deleteMember(Member member) {
+        memberRepository.delete(member);
     }
 
 }
