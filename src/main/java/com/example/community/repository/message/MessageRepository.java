7c5a07f732831f06b3420b7dfe356875dea77bb5
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-22 18:13:24 +0900

    feat: 쿼리문 추가

--- src/main/java/com/example/community/repository/message/MessageRepository.java
+++ src/main/java/com/example/community/repository/message/MessageRepository.java
@@ -2,6 +2,30 @@ package com.example.community.repository.message;
 
 import com.example.community.domain.message.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<Message,Long> {
+
+    @Query("select m from Message m join fetch m.receiver r " +
+        "where m.deletedByReceiver=false and r.username =:username")
+    List<Message> findAllByReceiverQuery(@Param("username") String username);
+
+    @Query("select m from Message m join fetch  m.receiver r " +
+        "where m.deletedByReceiver = false and m.id =:id and r.username=:username")
+    Optional<Message> findByIdWithReceiver(@Param("id") Long id, @Param("username") String username);
+
+    @Query("select m from Message m join fetch m.sender r " +
+        "where m.deletedBySender=false and r.username =:username")
+    List<Message> findAllBySenderQuery(@Param("username") String username);
+
+    @Query("select m from Message m join fetch  m.sender r " +
+        "where m.deletedBySender = false and m.id =:id and r.username=:username")
+    Optional<Message> findByIdWithSender(@Param("id") Long id, @Param("username") String username);
+
+
+
 }
