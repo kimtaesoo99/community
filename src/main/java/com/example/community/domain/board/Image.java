596ce33ba6c1479a021cd2ed7433d56c9fd08775
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-23 16:38:14 +0900

    test: 이미지 엔티티 추가

---
+++ src/main/java/com/example/community/domain/board/Image.java
@@ -0,0 +1,67 @@
+package com.example.community.domain.board;
+
+
+import com.example.community.domain.common.BaseEntity;
+import com.example.community.exception.UnsupportedImageFormatException;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.Arrays;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Image extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String uniqueName;
+
+    @Column(nullable = false)
+    private String originName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Board board;
+
+    private final static String[] supportedExtension = {"jpg", "jpeg", "gif", "bmp", "png"};
+
+    public Image(String originName) {
+        this.originName = originName;
+        this.uniqueName = generateUniqueName(extractExtension(originName));
+    }
+
+    public void initBoard(Board board) {
+        if(this.board == null) {
+            this.board = board;
+        }
+    }
+
+    private String generateUniqueName(String extension) {
+        return UUID.randomUUID().toString() + "." + extension;
+    }
+
+    private String extractExtension(String originName) {
+        try {
+            String ext = originName.substring(originName.lastIndexOf(".") + 1);
+            if(isSupportedFormat(ext)) return ext;
+        } catch (StringIndexOutOfBoundsException e) { }
+            throw new UnsupportedImageFormatException();
+    }
+
+    private boolean isSupportedFormat(String ext) {
+        return Arrays.stream(supportedExtension).anyMatch(e -> e.equalsIgnoreCase(ext));
+    }
+}
