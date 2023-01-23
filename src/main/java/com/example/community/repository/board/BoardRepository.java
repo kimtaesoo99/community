f1189fb66841988d19f7910bc8f1d62c82a8807e
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-23 16:36:47 +0900

    feat: 보드 리포지토리 추가

---
+++ src/main/java/com/example/community/repository/board/BoardRepository.java
@@ -0,0 +1,27 @@
+package com.example.community.repository.board;
+
+
+import com.example.community.domain.board.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface BoardRepository extends JpaRepository<Board,Long> {
+
+    @Query(value = "select b from Board b join fetch b.member m"
+        , countQuery = "select count(b) from Board b order by b.id desc ")
+    Page<Board> findAll(Pageable pageable);
+
+
+    @Query(value = "select b from Board b join fetch b.member m where b.title like '%title%'"
+        , countQuery = "select count(b) from Board b order by b.id desc")
+    Page<Board> findAllByTitleContaining(@Param("title") String keyword, Pageable pageable);
+
+}
+
+
+
+
