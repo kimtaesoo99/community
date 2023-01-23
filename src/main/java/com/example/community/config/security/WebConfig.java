3dd77110ec14852a5e0e3699fb96f4ff35e2d716
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-23 16:40:36 +0900

    feat: 이미지 저장에 대한 설정 추가

---
+++ src/main/java/com/example/community/config/security/WebConfig.java
@@ -0,0 +1,37 @@
+package com.example.community.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
+
+@EnableWebMvc
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final MessageSource messageSource;
+
+    private String location = "/Users/kimtaesoo/Desktop/image/";
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**")
+            .addResourceLocations("file:" + location)
+            .setCacheControl(CacheControl.maxAge(Duration.ofHours(1L)).cachePublic());
+    }
+
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+        return bean;
+    }
+}
