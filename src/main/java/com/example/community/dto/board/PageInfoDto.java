3aee37d6a2c6fa522cb0596443e44d7a4a5d57b4
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-23 16:39:57 +0900

    feat: 페이징 Dto 추가

---
+++ src/main/java/com/example/community/dto/board/PageInfoDto.java
@@ -0,0 +1,26 @@
+package com.example.community.dto.board;
+
+
+import com.example.community.domain.board.Board;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PageInfoDto {
+    private int totalPage; // 전체 페이지 수
+    private int nowPage; // 현재 페이지
+    private int numberOfElements; // 현재 페이지에 나올 데이터 수
+    private boolean isNext; // 다음 페이지 존재 여부
+
+    public PageInfoDto(Page<Board> result) {
+        this.totalPage = result.getTotalPages();
+        this.nowPage = result.getNumber();
+        this.numberOfElements = result.getNumberOfElements();
+        this.isNext = result.hasNext();
+    }
+}
+
