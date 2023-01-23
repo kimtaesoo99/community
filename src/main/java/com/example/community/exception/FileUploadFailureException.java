f6040e6bbe5b8c14855b01bb1f65fb24f8a3a856
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-23 16:43:43 +0900

    feat: 파일 업로드 실패 예외추가

--- src/main/java/com/example/community/exception/FileUploadFailureException.java
+++ src/main/java/com/example/community/exception/FileUploadFailureException.java
@@ -1,6 +1,7 @@
 package com.example.community.exception;
 
 public class FileUploadFailureException extends RuntimeException{
+
     public FileUploadFailureException(Throwable cause) {
         super(cause);
     }
