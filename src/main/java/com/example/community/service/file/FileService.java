ab999bf16e662decd21744c1728130e5843dba01
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-23 16:44:09 +0900

    feat: 파일 서비스 인터페이스 추가

--- src/main/java/com/example/community/service/file/FileService.java
+++ src/main/java/com/example/community/service/file/FileService.java
@@ -3,6 +3,7 @@ package com.example.community.service.file;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
+
     void upload(MultipartFile file, String filename);
     void delete(String filename);
 }
