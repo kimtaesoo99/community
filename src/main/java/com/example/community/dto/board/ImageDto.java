93089943ef9069f0a9e91a6d8212d2069c2cd34e
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-23 16:38:26 +0900

    test: 이미지 Dto 추가

---
+++ src/main/java/com/example/community/dto/board/ImageDto.java
@@ -0,0 +1,17 @@
+package com.example.community.dto.board;
+
+import com.example.community.domain.board.Image;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ImageDto {
+    private Long id;
+    private String originName;
+    private String uniqueName;
+
+    public static ImageDto toDto(Image image) {
+        return new ImageDto(image.getId(), image.getOriginName(), image.getUniqueName());
+    }
+}
