e8a57f85bf0f14aeca01dcba695f25b2a7252c3a
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-24 17:38:03 +0900

    feat: 좋아요 엔티티 추가

---
+++ src/main/java/com/example/community/domain/board/Likes.java
@@ -0,0 +1,37 @@
+package com.example.community.domain.board;
+
+import com.example.community.domain.common.BaseEntity;
+import com.example.community.domain.member.Member;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+@Entity
+public class Likes extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "likes_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    public Likes(Board board, Member member) {
+        this.board = board;
+        this.member = member;
+    }
+}
