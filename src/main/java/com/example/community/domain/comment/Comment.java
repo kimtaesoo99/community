5d33f6b0bc5fd3af7b0815af43ab885b2fc227e3
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-25 20:38:55 +0900

    feat: 댓글 엔티티 추가

---
+++ src/main/java/com/example/community/domain/comment/Comment.java
@@ -0,0 +1,53 @@
+package com.example.community.domain.comment;
+
+import com.example.community.domain.board.Board;
+import com.example.community.domain.common.BaseEntity;
+import com.example.community.domain.member.Member;
+import com.example.community.dto.comment.CommentEditRequestDto;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+@Entity
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(nullable = false)
+    @Lob
+    private String content;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Board board;
+
+    public Comment(String content, Member member, Board board) {
+        this.content = content;
+        this.member = member;
+        this.board = board;
+    }
+
+    public boolean isOwnComment(Member member) {
+        return this.member.equals(member);
+    }
+
+    public void edit(CommentEditRequestDto req){
+        this.content = req.getContent();
+    }
+}
