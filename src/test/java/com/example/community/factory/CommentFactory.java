4c2a41a2be99dc3175f8770682dfa37666fa122b
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-25 20:41:50 +0900

    test: 테스트용 댓글을 간단하게 만드는 메서드 추가

---
+++ src/test/java/com/example/community/factory/CommentFactory.java
@@ -0,0 +1,15 @@
+package com.example.community.factory;
+
+
+import com.example.community.domain.comment.Comment;
+import com.example.community.domain.member.Member;
+
+import static com.example.community.factory.BoardFactory.createBoard;
+
+public class CommentFactory {
+
+
+    public static Comment createComment(Member member) {
+        return new Comment(1L,"content", member, createBoard());
+    }
+}
