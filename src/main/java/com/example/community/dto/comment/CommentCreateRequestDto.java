dda25f5f3da90579040f8eb333e3c64371010cba
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-25 20:40:49 +0900

    feat: 댓글 생성 DTO 추가

---
+++ src/main/java/com/example/community/dto/comment/CommentCreateRequestDto.java
@@ -0,0 +1,26 @@
+package com.example.community.dto.comment;
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@ApiOperation(value = "댓글 생성 요청")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentCreateRequestDto {
+    @ApiModelProperty(value = "게시글 번호", notes = "게시글 번호를 입력해주세요.", required = true, example = "3")
+    @NotNull(message = "게시글 번호를 입력해주세요.")
+    @Positive(message = "게시글 번호를 입력해주세요.")
+    private Long boardId;
+
+    @ApiModelProperty(value = "댓글", notes = "댓글을 입력해주세요.", required = true, example = "example comment")
+    @NotBlank(message = "댓글을 입력해주세요.")
+    private String content;
+}
