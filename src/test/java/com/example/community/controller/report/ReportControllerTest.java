8c61397bf4298b6a134687c109d1547f2df05bf9
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-27 20:10:24 +0900

    test: 신고 컨트롤러 테스트 추가

---
+++ src/test/java/com/example/community/controller/report/ReportControllerTest.java
@@ -0,0 +1,114 @@
+package com.example.community.controller.report;
+
+import com.example.community.domain.member.Member;
+import com.example.community.dto.report.BoardReportRequestDto;
+import com.example.community.dto.report.CommentReportRequestDto;
+import com.example.community.dto.report.MemberReportRequestDto;
+import com.example.community.repository.member.MemberRepository;
+import com.example.community.service.report.ReportService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static com.example.community.factory.MemberFactory.createMember;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class ReportControllerTest {
+    @InjectMocks
+    ReportController reportController;
+
+    @Mock
+    ReportService reportService;
+
+    @Mock
+    MemberRepository memberRepository;
+
+    MockMvc mockMvc;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    public void beforeEach() {
+
+        mockMvc = MockMvcBuilders.standaloneSetup(reportController).build();
+    }
+
+    @Test
+    public void 유저신고_테스트() throws Exception {
+        // given
+        MemberReportRequestDto req = new MemberReportRequestDto(1L, "내용");
+
+        Member member = createMember();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(member.getId(), "", Collections.emptyList());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        given(memberRepository.findByUsername(authentication.getName())).willReturn(Optional.of(member));
+
+
+        // when, then
+        mockMvc.perform(
+                post("/api/reports/members")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(req)))
+            .andExpect(status().isOk());
+
+        verify(reportService).reportMember(member, req);
+    }
+
+    @Test
+    public void 게시판신고_테스트() throws Exception {
+        // given
+        BoardReportRequestDto req = new BoardReportRequestDto(1L, "내용");
+
+        Member member = createMember();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(member.getId(), "", Collections.emptyList());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        given(memberRepository.findByUsername(authentication.getName())).willReturn(Optional.of(member));
+
+
+        // when, then
+        mockMvc.perform(
+                post("/api/reports/boards")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(req)))
+            .andExpect(status().isOk());
+
+        verify(reportService).reportBoard(member, req);
+    }
+
+    @Test
+    public void 댓글신고_테스트() throws Exception {
+        // given
+        CommentReportRequestDto req = new CommentReportRequestDto(1L, "내용");
+
+        Member member = createMember();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(member.getId(), "", Collections.emptyList());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        given(memberRepository.findByUsername(authentication.getName())).willReturn(Optional.of(member));
+
+
+        // when, then
+        mockMvc.perform(
+                post("/api/reports/comments")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(req)))
+            .andExpect(status().isOk());
+
+        verify(reportService).reportComment(member, req);
+    }
+}
