ddb219502b56650a741f69a7585233f8085df6ac
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-27 20:07:01 +0900

    feat: 댓글 신고 요청 DTO 추가

---
+++ src/main/java/com/example/community/dto/report/CommentReportRequestDto.java
@@ -0,0 +1,25 @@
+package com.example.community.dto.report;
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@ApiOperation(value = "댓글 신고 처리 요청")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentReportRequestDto {
+    @ApiModelProperty(value = "신고할 댓글의 아이디", notes = "신고할 댓글의 아이디를 입력해주세요.", required = true, example = "1")
+    @NotNull(message = "신고할 유저의 아이디 입력해주세요.")
+    private Long reportedCommentId;
+
+    @ApiModelProperty(value = "신고 사유", notes = "신고 사유를 입력해주세요.", required = true, example = "Insulting")
+    @NotBlank(message = "신고 사유를 입력하세요.")
+    private String content;
+
+}
