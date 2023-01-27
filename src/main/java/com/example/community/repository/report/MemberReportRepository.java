f6be790c14cba9e30694a3eb472cffd1504af5e6
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-27 20:08:45 +0900

    feat: 유저 신고 리포지토리 추가

---
+++ src/main/java/com/example/community/repository/report/MemberReportRepository.java
@@ -0,0 +1,12 @@
+package com.example.community.repository.report;
+
+import com.example.community.domain.member.Member;
+import com.example.community.domain.report.MemberReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberReportRepository extends JpaRepository<MemberReport,Long> {
+    boolean existsByReporterAndReportedMember(Member member, Member reportedMember);
+    List<MemberReport> findAllByReportedMember(Member reportedMember);
+}
