653d5edca583465c5b307112129bbbd6112344b4
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-27 20:08:54 +0900

    feat: 댓글 신고 리포지토리 추가

---
+++ src/main/java/com/example/community/repository/report/CommentReportRepository.java
@@ -0,0 +1,16 @@
+package com.example.community.repository.report;
+
+
+import com.example.community.domain.comment.Comment;
+import com.example.community.domain.member.Member;
+import com.example.community.domain.report.CommentReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentReportRepository extends JpaRepository<CommentReport,Long> {
+    boolean existsByReporterAndReportedComment(Member member, Comment reportedComment);
+
+    List<CommentReport> findAllByReportedComment(Comment reportedComment);
+}
+
