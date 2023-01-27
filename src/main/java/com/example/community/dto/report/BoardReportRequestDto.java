8f184d5a295873c24e1351d132af22a50d5319bd
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-27 20:06:52 +0900

    feat: 게시판 신고 요청 DTO 추가

---
+++ src/main/java/com/example/community/dto/report/BoardReportRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.community.dto.report;
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@ApiOperation(value = "게시글 신고 처리 요청")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardReportRequestDto {
+    @ApiModelProperty(value = "신고 할 게시글 아이디", notes = "신고할 게시글 아이디를 입력해주세요.", required = true, example = "1")
+    @NotNull(message = "신고할 게시글의 아이디를 입력해주세요.")
+    private Long reportedBoardId;
+
+    @ApiModelProperty(value = "신고 사유", notes = "신고 사유를 입력해주세요.", required = true, example = "Insulting")
+    @NotBlank(message = "신고 사유를 입력하세요. ")
+    private String content;
+}
