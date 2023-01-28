92b0a53f276e7733f31929dbb904b4dc4baaf629
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-28 18:31:23 +0900

    feat: 정지목록을 조회하는 쿼리 추가

--- src/main/java/com/example/community/repository/member/MemberRepository.java
+++ src/main/java/com/example/community/repository/member/MemberRepository.java
@@ -2,7 +2,9 @@ package com.example.community.repository.member;
 
 import com.example.community.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -10,5 +12,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByUsernameAndPassword(String username, String password);
     Optional<Member> findByUsername(String username);
     public boolean existsByUsername(String username);
+
+    @Query("select m from Member m where m.isReported =true")
+    List<Member> findAllByReportedIsTrue();
 
 }
