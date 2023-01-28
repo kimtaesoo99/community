8ba011157f028ab474ee4a1a0daf10d8f1d5f0df
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-28 18:31:18 +0900

    feat: 정지목록을 조회하는 쿼리 추가

--- src/main/java/com/example/community/repository/comment/CommentRepository.java
+++ src/main/java/com/example/community/repository/comment/CommentRepository.java
@@ -2,9 +2,13 @@ package com.example.community.repository.comment;
 
 import com.example.community.domain.comment.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     List<Comment> findAllByBoardId(Long boarId);
+
+    @Query("select c from Comment c where c.isReported =true")
+    List<Comment> findAllByReportedIsTrue();
 }
