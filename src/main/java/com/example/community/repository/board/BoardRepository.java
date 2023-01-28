b7c574d1f3cd82ac8aa800e9b0fac93d28d06404
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-28 18:29:53 +0900

    feat: 신고당한 게시판정보를 조회하는 쿼리 추가

--- src/main/java/com/example/community/repository/board/BoardRepository.java
+++ src/main/java/com/example/community/repository/board/BoardRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
 
@@ -20,6 +22,8 @@ public interface BoardRepository extends JpaRepository<Board,Long> {
         , countQuery = "select count(b) from Board b order by b.id desc")
     Page<Board> findAllByTitleContaining(@Param("title") String keyword, Pageable pageable);
 
+    @Query("select b from Board b where b.isReported =true")
+    List<Board> findAllByReportedIsTrue();
 }
 
 
