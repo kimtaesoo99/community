a46d14e79064a4c6872ed5a7aa750c96e5a9d515
kimtaesoo <kimtaesoo7@naver.com>
2023-05-21 21:29:27 +0900

    feat: HandlerMethodArgumentResolver를 사용하여 @Login 어노테이션을 생성

---
+++ src/main/java/com/example/community/config/guard/LoginMemberArgumentResolver.java
@@ -0,0 +1,35 @@
+package com.example.community.config.guard;
+
+import com.example.community.domain.member.Member;
+import com.example.community.exception.MemberNotFoundException;
+import com.example.community.repository.member.MemberRepository;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberRepository memberRepository;
+
+    public LoginMemberArgumentResolver(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Login.class);
+    }
+
+    @Override
+    public Member resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = SecurityContextHolder.createEmptyContext().getAuthentication();
+        return memberRepository.findByUsername(authentication.getName())
+            .orElseThrow(MemberNotFoundException::new);
+    }
+}
